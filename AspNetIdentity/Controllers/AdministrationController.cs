3f70422fcecd5a07a6dba65c1e7f08324305a8e9
unknown <zubairshakoor6@gmail.com>
2019-12-03 21:46:02 +0500

    User and Role creation using Identity Done

---
+++ AspNetIdentity/Controllers/AdministrationController.cs
@@ -0,0 +1,53 @@
+﻿using AspNetIdentity.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetIdentity.Controllers
+{
+    [Authorize]
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole role = new IdentityRole()
+                {
+                    Name = model.Name
+                };
+                IdentityResult result = await _roleManager.CreateAsync(role);
+
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");                
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View();
+        }
+    }
+}
+;
