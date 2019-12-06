fa4c3c6fac6b49205b15281a75ead3a3b88b3521
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2019-12-06 17:43:34 +0500

    code added

---
+++ AspNetIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
